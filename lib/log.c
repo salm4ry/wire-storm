59721c4cb6d2cb7b1e338d57cafd7925bf96fc3b
Salma Berriche <salma.berriche@gmail.com>
2025-08-16 15:28:05 +0100

    Add p_error macro
    p_error() prints error message along with file, function and line
    (designed for debugging)

--- lib/log.c
+++ lib/log.c
@@ -1,9 +1,7 @@
 /// @file
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <time.h>
 
 #include "log.h"
--- lib/log.h
+++ lib/log.h
@@ -1,10 +1,25 @@
 /// @file
 
+#include <stdio.h>
+#include <string.h>  /* required for p_error macro */
 #include <stdarg.h>
 
 #define MAX_TIME_STR 21  ///< maximum length for log message timestamp
 #define TIME_FMT "%Y-%m-%d %H:%M:%S " ///< log message timestamp format
 #define MAX_LOG_MSG 128  ///< maximum log message length
+
+/**
+ * @brief Print error message
+ * @param msg output message
+ * @param err_code error code (converted to message using `strerror()`)
+ * @details Includes file, function and line details (hence a macro)
+ */
+#define p_error(msg, err_code) \
+{ \
+	fprintf(stderr, "%s:%s:%d: %s: %s\n", \
+			__FILE__, __func__, __LINE__, \
+			msg, strerror(err_code));\
+}
 
 void format_time(char *time_str);
 void format_msg(char *msg, char *fmt, va_list args);
